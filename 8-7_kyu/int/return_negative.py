9023ad8725951729bfe6727d2484e8f0f117843c
Emanuele Zignale <e.zignale@innonation.it>
2025-11-05 15:45:56 +0100

    more solution

---
+++ 8-7_kyu/int/return_negative.py
@@ -0,0 +1,37 @@
+'''
+<summary>
+    In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+    Examples
+    make_negative(1);  # return -1
+    make_negative(-5); # return -5
+    make_negative(0);  # return 0
+    Notes
+    The number can be negative already, in which case no change is required.
+    Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+    Fundamentals
+
+    level 8kyu
+
+</summary>
+'''
+
+def make_negative( number ):
+    if number>0:
+        return -number
+    if number <0:
+        return number
+    if number == 0:
+        return 0
+
+
+def make_negative(number):
+    return -number if number > 0 else number
+
+def main():
+    print(make_negative(-1))
+    print(make_negative(-5))
+    print(make_negative(0))
+
+if __name__ == "__main__":
+    main()
